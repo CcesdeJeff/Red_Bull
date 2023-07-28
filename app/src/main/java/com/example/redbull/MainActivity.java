bd69c52d804f06f1e1300e832090cfc24b4bccb9
GomezBra <brayan-0123@hotmail.com>
2023-07-28 08:53:25 -0500

    Se realiza cambios

--- app/src/main/java/com/example/redbull/MainActivity.java
+++ app/src/main/java/com/example/redbull/MainActivity.java
@@ -2,13 +2,33 @@ package com.example.redbull;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button buttonIngresar, buttonRegistrar;
+
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        buttonIngresar=findViewById(R.id.buttonIngresar);
+        buttonRegistrar=findViewById(R.id.buttonRegistrar);
+
+
+        buttonIngresar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this , Ingresar.class);
+                startActivity(intent);
+            }
+        });
     }
 }
